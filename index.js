a77adcde8bc491017e1936525b9a6c08fe7c410d
unknown <nangelov1990@gmail.com>
2020-03-07 03:01:36 +0200

    índex.js by standard

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let http = require('http')
-let port = 1337
+const http = require('http')
+const port = 1337
 
 http
-    .createServer((req, res) => {
-        res.writeHead(200)
-        res.write('Hi from NodeJS')
-        res.end()
-    })
-    .listen(port)
+  .createServer((req, res) => {
+    res.writeHead(200)
+    res.write('Hi from NodeJS')
+    res.end()
+  })
+  .listen(port)
 
 console.log(`Server listening on port ${port}`)
